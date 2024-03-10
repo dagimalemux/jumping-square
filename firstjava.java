9f6f7c2a91b4a32e797b976a8a465351dad3f27d
Dagim Alemu <dagimalemux@gmail.com>
2024-03-10 11:23:34 +0300

    instance varibale and static variable difference

--- firstjava.java
+++ firstjava.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class firstjava{
+
+    // instance Variables 
+
+    String city; // instance variable by defalult a variable we declare in the class is an instance variable;
+
+    static String country; // static variable;
+
+    // difference between static and instance .... Instance variables depend on the object and static varibles on the class;
     public static void main(String[] args){
         char gender;
 
